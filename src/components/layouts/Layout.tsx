452489734c9a00e376ca650f30b3fdce8a8eb765
Jorge Calderon <jorge.calderon@globant.com>
2023-02-07 11:35:36 -0600

    metatag

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,5 +1,5 @@
 import { FC, PropsWithChildren } from 'react'
-import { Container, Card, Row, Text, Spacer } from "@nextui-org/react";
+import { Container } from "@nextui-org/react";
 import Head from "next/head"
 import { Navbar } from "@/components/ui"
 
@@ -7,14 +7,21 @@ interface Props {
     title?: string
 }
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
+
     return (
         <>
             <Head>
                 <title>{title || "Pokemon App"}</title>
                 <meta name="author" content="Jorge Calderon Peralta" />
-                <meta name="description" content="Informacion sobre el pokemon XXXXX" />
-                <meta name="keywords" content="XXXX, pokemon, pokedex" />
+                <meta name="description" content="Informacion sobre el pokemon" />
+                <meta name="keywords" content=" pokemon, pokedex" />
+
+                <meta property="og:title" content={`Información sobre el pokémon ${title}`} />
+                <meta property="og:description" content={`Esta es la pagina sobre ${title}`} />
+                <meta property="og:image" content={`${origin}/img/banner.png`} />
             </Head>
             <Navbar />
             <Container as='main' lg>
